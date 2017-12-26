c6d6ffda234c44f5d7a8048b572ac9d21e95f0a5
Gounaya Oussama <oussama.gounaya@etudiant.univ-lille1.fr>
2017-12-26 21:18:46 +0100

    add TicTac

--- src/TicTacGame/XOButton.java
+++ src/TicTacGame/XOButton.java
@@ -1,5 +1,6 @@
 package TicTacGame;
 
+
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
@@ -7,20 +8,32 @@ import java.awt.event.ActionEvent;
 
 public class XOButton extends JButton implements ActionListener{
     ImageIcon X,O;
-    byte value= 0;
-    /*
-    0 : nothing
-    1 : X
-    2 : O
-     */
+    byte value=0;
+	/*
+	0:nothing
+	1:X
+	2:O
+	*/
 
     public XOButton(){
-        X= new ImageIcon(this.getClass().getResource("X.png"));
-        O= new ImageIcon(this.getClass().getResource("Y.png"));
+        X=new ImageIcon(this.getClass().getResource("X.png"));
+        O=new ImageIcon(this.getClass().getResource("O.png"));
         this.addActionListener(this);
     }
-    public void ActionPerformed(ActionEvent e){
 
+    public void actionPerformed(ActionEvent e){
+        value++;
+        value%=3;
+        switch(value){
+            case 0:
+                setIcon(null);
+                break;
+            case 1:
+                setIcon(X);
+                break;
+            case 2:
+                setIcon(O);
+                break;
+        }
     }
-
 }
