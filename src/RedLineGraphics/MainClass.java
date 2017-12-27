995f0b257db675c3f3dc2e19f9a8234ac35de3d7
Gounaya Oussama <oussama.gounaya@etudiant.univ-lille1.fr>
2017-12-27 20:26:57 +0100

    add Red line & Red ball

---
+++ src/RedLineGraphics/MainClass.java
@@ -0,0 +1,7 @@
+package RedLineGraphics;
+
+public class MainClass {
+    public static void main(String[] args){
+        new Fenetre();
+    }
+}
--- src/TicTacGame/TictacGame.java
+++ src/TicTacGame/TictacGame.java
@@ -25,8 +25,6 @@ public class TictacGame extends JFrame{
         setVisible(true);
 
     }
-
-
 }
 
 
